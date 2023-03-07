9b34f1188fd2ad8976a33959c6db702f93243bb1
OzPol <95313700+OzPol@users.noreply.github.com>
2023-03-06 23:28:55 -0500

    Adding Code

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
-# Initial GitHub Commit
-print("test")
+"""Ozlem Polat"""
 
+def encode(original_password):
+    encoded_password = ""
+    for digit in original_password:
+        new_digit = str((int(digit) + 3) % 10)
+        encoded_password += new_digit
+    return encoded_password
+
+def decode(encoded_password):
+    pass
+
+def main():
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit\n")
+        option = input("Please enter an option: ")
+        if option == "1":
+            original_password = input("Please enter your password to encode: ")
+            encoded_password = encode(original_password)
+            print("Your password has been encoded and stored!\n")
+            
+        elif option == "2":
+            decoded_password = decode(encoded_password)
+            print("The encoded password is", encoded_password, "and the original password is "+decoded_password+".\n")
+        elif option == "3":            
+            break
+        else:
+            print("Invalid option, please enter 1-3.")
+
+if __name__ == "__main__":
+    main()
